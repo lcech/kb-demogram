b6fc339ede3e0fa728deda56a227a1ad5bf84796
Lukas Cech <ja@lukascech.cz>
2016-04-15 17:08:48 +0200

    data-attribute-less wizard tracking

--- js/wizard.js
+++ js/wizard.js
@@ -1,9 +1,10 @@
-$(':input[data-fieldname]').change(function(event) {
+/*global $*/
+$(':input').change(function(event) {
   var $target;
   $target = $(event.target);
   measure({
     event: 'inputChange',
-    fieldName: $target.data('fieldname'),
+    fieldName: $("label[for=" + $target.attr("id") + "]").text(),
     fieldValue: $target.val()
   });
 });
